dd7148bacea025b4a3fd025681a9736843503776
yusuf kilic <yusufkilic811@gmail.com>
2024-11-12 21:11:55 +0300

    Initial commit

---
+++ src/test/java/_01_ApiTest.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+
+public class _01_ApiTest {
+
+
+    @Test
+    public void Test1() {
+        given().
+
+                when().
+
+
+                then()
+
+
+        ;
+    }
+}
